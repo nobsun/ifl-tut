a11352f49a423c0a2b1ed0ca522563bbe1d1c741
Nobuo Yamashita <nobsun@sampou.org>
2026-02-21 18:12:40 +0900

    separate SeparateLambda from FullLazy

---
+++ src/Lifter/Mark4/SeparateLambda.hs
@@ -0,0 +1,38 @@
+-- # Lifter.Mark4.SeparateLambda
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Lifter.Mark4.SeparateLambda
+    ( separateLams
+    ) where
+
+import Data.Functor.Foldable
+
+import Language hiding (pprint, )
+import Lambda
+
+-- ### 6.6.5 Separating the lambdas
+
+separateLams :: CoreProgram -> CoreProgram
+separateLams prog
+    = [ (name, [], mkSepLams args (separateLamsExpr rhs))
+      | (name, args, rhs) <- prog ]
+
+separateLamsExpr :: CoreExpr -> CoreExpr
+separateLamsExpr = cataExpr phi where
+    phi = \ case
+        EVarF v         -> EVar v
+        ENumF n         -> ENum n
+        EConstrF t a    -> EConstr t a
+        EApF e1 e2      -> EAp e1 e2
+        ECaseF e alts   -> ECase e alts
+        ELamF args body -> mkSepLams args body
+        ELetF isRec defns body -> ELet isRec defns body
+
+mkSepLams :: [a] -> Expr a -> Expr a
+mkSepLams args body = cata phi args where
+    phi = \ case
+        Nil            -> body
+        Cons arg body' -> ELam [arg] body'
